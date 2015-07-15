96f8c8aa146b648a887126921489528e0bff3de5
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-14 19:59:03 -1000

    last 5

--- todo_app.sql
+++ todo_app.sql
@@ -1,8 +1,8 @@
-DELETE FROM pg_user WHERE usename = 'michael';
+DROP USER 'michae';
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 DROP DATABASE todo_app;
 CREATE DATABASE todo_app ENCODING 'utf8';
-\connect {todo_app | conninfo};
+\connect todo_app;
 
 CREATE TABLE tasks(
   id serial NOT NULL,
@@ -10,14 +10,28 @@ CREATE TABLE tasks(
   description text,
   created_at timestamp without time zone NOT NULL DEFAULT NOW(),
   updated_at timestamp without time zone,
-  completed boolean NOT NULL DEFAULT NULL
+  completed boolean NOT NULL,
   PRIMARY KEY (id)
 );
 
 ALTER TABLE tasks DROP COLUMN completed RESTRICT;
+ALTER TABLE tasks
+  ADD COLUMN completed_at timestamp DEFAULT NULL;
+ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL DEFAULT NOW();
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT NOW();
 
+INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+INSERT INTO tasks (title, description) VALUES ('Study PostgreSQL', 'Read all the documentation');
 
+SELECT title FROM tasks WHERE completed_at IS null;
 
+UPDATE tasks SET completed_at = now() WHERE title = 'Study SQL';
 
-ALTER DATABASE todo_app OWNER TO michael ENCODING 'utf8';
+SELECT title, description from tasks WHERE completed_at IS null;
+
+SELECT * FROM tasks ORDER BY created_at DESC;
+
+INSERT INTO tasks (title, description) VALUES ('mistake 1', 'a test entry');
+
+INSERT INTO tasks (title, description) VALUES ('mistake 2', 'another test entry');
 
