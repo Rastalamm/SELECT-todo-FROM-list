57cac4181b942422e30279363e5e614216112fa1
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-14 19:02:33 -1000

    completed first 7

--- todo_app.sql
+++ todo_app.sql
@@ -2,9 +2,7 @@ DELETE FROM pg_user WHERE usename = 'michael';
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 DROP DATABASE todo_app;
 CREATE DATABASE todo_app ENCODING 'utf8';
-
-
-
+\connect {todo_app | conninfo};
 
 CREATE TABLE tasks(
   id serial NOT NULL,
@@ -13,4 +11,13 @@ CREATE TABLE tasks(
   created_at timestamp without time zone NOT NULL DEFAULT NOW(),
   updated_at timestamp without time zone,
   completed boolean NOT NULL DEFAULT NULL
+  PRIMARY KEY (id)
 );
+
+ALTER TABLE tasks DROP COLUMN completed RESTRICT;
+
+
+
+
+ALTER DATABASE todo_app OWNER TO michael ENCODING 'utf8';
+
